79808e5b164b60ef69d9fc336afba7123590bc81
ilyastunch <iltun85@gmail.com>
2021-02-05 23:22:21 +0300

    working on hour 1

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,7 @@ public class Main {
 	// write your code here
         System.out.println("Hello Git Last Day");
         //We are exited!!!
+        //We are dedicated.
+        //We are successful testers
     }
 }
